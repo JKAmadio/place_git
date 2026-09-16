dc901d106a4202e23d4296abedd7d15861bd1dd5
Julia Amadio <jkahvedjian@gmail.com>
2026-09-16 15:49:23 -0300

    Adiciona validacao minima de senha

--- auth.py
+++ auth.py
@@ -1,3 +1,7 @@
 def autenticar(usuario, senha):
     # TODO: validar contra base de usuarios real
-    return bool(usuario) and bool(senha)
+    if not usuario or not senha:
+        return False
+    if len(senha) < 8:
+        return False
+    return True
