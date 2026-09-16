68438848c4b70d29614bdfdf70a4a56afb3e95da
Julia Amadio <jkahvedjian@gmail.com>
2026-09-16 15:52:31 -0300

    Adiciona funcao auxiliar de descricao do desconto

--- desconto.py
+++ desconto.py
@@ -1,3 +1,7 @@
 def aplicar_desconto(preco, percentual):
     valor = preco + (preco * percentual / 100)
     return round(valor, 2)
+
+
+def descricao_desconto(percentual):
+    return f"Desconto de {percentual}% aplicado"
